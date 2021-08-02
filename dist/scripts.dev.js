0f0f9e872f9b52d24d90299ce7d6cec8c7c4b57e
Lajos Bíró <biro_lajos@freemail.hu>
2021-08-02 16:05:54 +0200

    2021.08.02
    Fejlesztés második commit-ig

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -5,17 +5,38 @@ fetch("adatok.json").then(function (response) {
 }).then(function (data) {
   //console.log(data[0].img.data.$binary.base64)
   //let kep = window.atob(data[0].img.data.$binary.base64)
+  var currentPhoto = 0;
+  var v_image;
 
-  /*
-              for (var i = 0; i < data.length; i++) {
-  
-                  var img = document.createElement('img');
-                  img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
-                  document.body.appendChild(img);
-  
-              }
-  */
-  bigImage.src = 'data:image/png;base64,' + data[0].img.data.$binary.base64; // document.body.appendChild(bigImage);
+  for (var i = 0; i < data.length; i++) {
+    /* 
+    var img = document.createElement('img');
+    img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
+    document.body.appendChild(img);
+    */
+    v_image = '<img id="img_' + i + '" src =' + 'data:image/png;base64,' + data[i].img.data.$binary.base64 + '></img>';
+    $("#indexSection").append(v_image); //console.log(v_image);  
+  }
 
-  document.section.appendChild(bigImage);
+  $("img").hover(function () {
+    $(this).css("cursor", "pointer");
+  });
+  $("#bigImage").hover(function () {
+    $(this).css("cursor", "context-menu");
+  });
+  $("img").on("click", function () {
+    var v_index = $(this).attr('id');
+    v_index = v_index.substr(v_index.indexOf("_") + 1);
+    currentPhoto = v_index;
+    bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
+    $('#bigText').text(data[currentPhoto].name);
+    console.log(currentPhoto); //console.log(data.length);
+  });
+  $("#bigImage").click(function () {
+    ;
+  });
+  bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
+  $('#bigText').text(data[currentPhoto].name); // a kép áttűnésnél járok
+  // http://css3.bradshawenterprises.com/cfimg/
+  // css image fade
 });
--- scripts.js
+++ scripts.js
@@ -4,22 +4,55 @@ fetch("adatok.json")
                 //console.log(data[0].img.data.$binary.base64)
                 //let kep = window.atob(data[0].img.data.$binary.base64)
 
-/*
-            for (var i = 0; i < data.length; i++) {
+            var currentPhoto = 0;
+            let v_image;
 
+            for (var i = 0; i < data.length; i++) {
+                /* 
                 var img = document.createElement('img');
                 img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
                 document.body.appendChild(img);
+                */
+                v_image =  '<img id="img_'+ i +'" src ='+ 'data:image/png;base64,' + data[i].img.data.$binary.base64 +'></img>';
+                $("#indexSection").append(v_image); 
+
+                //console.log(v_image);  
 
             }
-*/
+
+            $("img").hover(function() {
+                $(this).css("cursor", "pointer")
+            })
+
+            $("#bigImage").hover(function() {
+                $(this).css("cursor", "context-menu")
+            })
 
 
+            $("img").on("click", function() {                
+                let v_index = $(this).attr('id');
+                v_index = v_index.substr(v_index.indexOf("_")+1);
+                currentPhoto = v_index;
+                
+                bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
+                $('#bigText').text(data[currentPhoto].name);
 
-            bigImage.src = 'data:image/png;base64,' + data[0].img.data.$binary.base64;
-            // document.body.appendChild(bigImage);
-            document.section.appendChild(bigImage);
+                console.log(currentPhoto);
+                
+                //console.log(data.length);
+            });    
 
+            $("#bigImage").click(() => {
+               ;
+            });    
+
+            bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
+            $('#bigText').text(data[currentPhoto].name);
+
+
+// a kép áttűnésnél járok
+// http://css3.bradshawenterprises.com/cfimg/
+// css image fade
 
 
 });
