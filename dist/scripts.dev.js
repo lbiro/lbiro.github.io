03edbc56e9db264106743a1eeb350c87e4838aad
Lajos Bíró <biro_lajos@freemail.hu>
2021-08-02 11:17:40 +0200

    Fejlesztés 2021.08.02
    A képeket sikerült beolvasni JSON base64 ből

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -5,12 +5,17 @@ fetch("adatok.json").then(function (response) {
 }).then(function (data) {
   //console.log(data[0].img.data.$binary.base64)
   //let kep = window.atob(data[0].img.data.$binary.base64)
-  var kep = data[0].img.data.toString('base64');
-  var img = document.createElement('img'); //img.src = 'data:image/png;base64,' + data[0].img.data.$binary.base64;
 
-  img.src = 'data:image/png;base64,' + kep.toString('base64'); //  "data:image/<%=image.img.contentType%>;base64, <%=image.img.data.toString('base64')%>"
+  /*
+              for (var i = 0; i < data.length; i++) {
+  
+                  var img = document.createElement('img');
+                  img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
+                  document.body.appendChild(img);
+  
+              }
+  */
+  bigImage.src = 'data:image/png;base64,' + data[0].img.data.$binary.base64; // document.body.appendChild(bigImage);
 
-  document.body.appendChild(img);
-  console.log(data[0].name);
-  console.log(data[0].img.data);
+  document.section.appendChild(bigImage);
 });
