db2f81c9e2a62de6af2e159f3a3f1b8ccb00f056
Lajos Bíró <biro_lajos@freemail.hu>
2021-08-03 14:47:34 +0200

    Commit 2021.08.03
    Index léptetés

--- dist/scripts.dev.js
+++ dist/scripts.dev.js
@@ -3,40 +3,100 @@
 fetch("adatok.json").then(function (response) {
   return response.json();
 }).then(function (data) {
-  //console.log(data[0].img.data.$binary.base64)
-  //let kep = window.atob(data[0].img.data.$binary.base64)
-  var currentPhoto = 0;
-  var v_image;
+  var newPhoto = $("#current").text();
+  var newImgId = "img_" + $("#current").text();
+  var currentPhoto;
+  var currentImgId;
 
-  for (var i = 0; i < data.length; i++) {
-    /* 
-    var img = document.createElement('img');
-    img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
-    document.body.appendChild(img);
-    */
-    v_image = '<img id="img_' + i + '" src =' + 'data:image/png;base64,' + data[i].img.data.$binary.base64 + '></img>';
-    $("#indexSection").append(v_image); //console.log(v_image);  
+  function init() {
+    var v_image;
+
+    for (var i = 0; i < data.length; i++) {
+      v_image = '<img id="img_' + i + '" class = "indexImages tooltip" src =' + 'data:image/png;base64,' + data[i].img.data.$binary.base64 + '></img>';
+      $("#indexSection").append(v_image);
+      v_image = '<span class="tooltiptext">' + data[i].name + '</span>';
+      $("#img_" + i).append(v_image);
+    }
+
+    ;
+    fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
   }
 
-  $("img").hover(function () {
+  ;
+  init();
+
+  function fillbigImage(pNewPhoto, pNewImgId, pCurrentPhoto, pCurrentImgId) {
+    bigImage.src = 'data:image/png;base64,' + data[pNewPhoto].img.data.$binary.base64;
+    $('#bigText').text(data[pNewPhoto].name);
+    var x = document.getElementById(pNewImgId);
+    $(x).css("width", "102px");
+    $(x).css("height", "112px");
+    $(x).css("box-shadow", "5px 5px 5px #ccc");
+    $(x).css("-moz-box-shadow", "5px 5px 5px #ccc");
+    $(x).css("-webkit-box-shadow", "5px 5px 5px #ccc");
+    $(x).css("-khtml-box-shadow", "5px 5px 5px #ccc");
+
+    if (pNewPhoto != pCurrentPhoto) {
+      var x = document.getElementById(pCurrentImgId);
+      $(x).css("width", "82px");
+      $(x).css("height", "92px");
+      $(x).css("box-shadow", "0px 0px 0px #ccc");
+      $(x).css("-moz-box-shadow", "0px 0px 0px #ccc");
+      $(x).css("-webkit-box-shadow", "0px 0px 0px #ccc");
+      $(x).css("-khtml-box-shadow", "0px 0px 0px #ccc");
+    }
+
+    $("#current").text(newPhoto);
+  }
+
+  ;
+  $(".indexImages").hover(function () {
     $(this).css("cursor", "pointer");
   });
   $("#bigImage").hover(function () {
     $(this).css("cursor", "context-menu");
   });
-  $("img").on("click", function () {
+  $(".indexImages").on("click", function () {
+    currentImgId = newImgId;
+    currentPhoto = newPhoto;
     var v_index = $(this).attr('id');
+    newImgId = v_index;
     v_index = v_index.substr(v_index.indexOf("_") + 1);
-    currentPhoto = v_index;
-    bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
-    $('#bigText').text(data[currentPhoto].name);
-    console.log(currentPhoto); //console.log(data.length);
+    newPhoto = v_index;
+    fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
   });
-  $("#bigImage").click(function () {
-    ;
+  $("#rightArrow").click(function () {
+    newPhoto = $("#current").text();
+    newImgId = "img_" + $("#current").text();
+    currentImgId = newImgId;
+    currentPhoto = newPhoto;
+
+    if (newPhoto <= data.length - 2) {
+      ++newPhoto;
+      newImgId = "img_" + newPhoto.toString();
+    } else {
+      newPhoto = 0;
+      newImgId = "img_" + newPhoto.toString();
+    }
+
+    fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
   });
-  bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
-  $('#bigText').text(data[currentPhoto].name); // a kép áttűnésnél járok
+  $("#leftArrow").click(function () {
+    newPhoto = $("#current").text();
+    newImgId = "img_" + $("#current").text();
+    currentImgId = newImgId;
+    currentPhoto = newPhoto;
+
+    if (newPhoto != 0) {
+      --newPhoto;
+      newImgId = "img_" + newPhoto.toString();
+    } else {
+      newPhoto = data.length - 1;
+      newImgId = "img_" + newPhoto.toString();
+    }
+
+    fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
+  }); // a kép áttűnésnél járok
   // http://css3.bradshawenterprises.com/cfimg/
   // css image fade
 });
--- scripts.js
+++ scripts.js
@@ -1,53 +1,134 @@
 fetch("adatok.json")
             .then(response => response.json())
             .then(data => {
-                //console.log(data[0].img.data.$binary.base64)
-                //let kep = window.atob(data[0].img.data.$binary.base64)
 
-            var currentPhoto = 0;
-            let v_image;
+            var newPhoto = $("#current").text();
+            var newImgId = "img_"+$("#current").text();
+            var currentPhoto;
+            var currentImgId;
 
-            for (var i = 0; i < data.length; i++) {
-                /* 
-                var img = document.createElement('img');
-                img.src = 'data:image/png;base64,' + data[i].img.data.$binary.base64;
-                document.body.appendChild(img);
-                */
-                v_image =  '<img id="img_'+ i +'" src ='+ 'data:image/png;base64,' + data[i].img.data.$binary.base64 +'></img>';
-                $("#indexSection").append(v_image); 
 
-                //console.log(v_image);  
 
-            }
+            function init(){
 
-            $("img").hover(function() {
-                $(this).css("cursor", "pointer")
-            })
+                let v_image;
+
+                for (var i = 0; i < data.length; i++) {
+
+                    v_image =  '<img id="img_'+ i +'" class = "indexImages tooltip" src ='+ 'data:image/png;base64,' + data[i].img.data.$binary.base64 +'></img>';
+                    $("#indexSection").append(v_image); 
+
+                    v_image =  '<span class="tooltiptext">'+ data[i].name + '</span>'; 
+                    $("#img_"+i).append(v_image); 
+
+                };
+
+                fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
+
+            };
+
+            init();
+
+            function fillbigImage(pNewPhoto, pNewImgId, pCurrentPhoto, pCurrentImgId) {
+
+                bigImage.src = 'data:image/png;base64,' + data[pNewPhoto].img.data.$binary.base64;
+                $('#bigText').text(data[pNewPhoto].name);
+
+                var x=document.getElementById(pNewImgId);
+                $(x).css("width","102px");
+                $(x).css("height","112px");
+                $(x).css("box-shadow","5px 5px 5px #ccc");
+                $(x).css("-moz-box-shadow","5px 5px 5px #ccc");
+                $(x).css("-webkit-box-shadow","5px 5px 5px #ccc");
+                $(x).css("-khtml-box-shadow","5px 5px 5px #ccc");
+
+                if ( pNewPhoto != pCurrentPhoto ) {
+
+                    var x=document.getElementById(pCurrentImgId);
+                    $(x).css("width","82px");
+                    $(x).css("height","92px");
+                    $(x).css("box-shadow","0px 0px 0px #ccc");
+                    $(x).css("-moz-box-shadow","0px 0px 0px #ccc");
+                    $(x).css("-webkit-box-shadow","0px 0px 0px #ccc");
+                    $(x).css("-khtml-box-shadow","0px 0px 0px #ccc"); 
+                }
+
+                $("#current").text(newPhoto);
+
+            };
+
+            $(".indexImages").hover(function() {
+                $(this).css("cursor", "pointer");
+            });
 
             $("#bigImage").hover(function() {
-                $(this).css("cursor", "context-menu")
-            })
+                $(this).css("cursor", "context-menu");
+            });
 
 
-            $("img").on("click", function() {                
+            $(".indexImages").on("click", function() {    
+                
+                currentImgId = newImgId;
+                currentPhoto = newPhoto;
+
+
                 let v_index = $(this).attr('id');
+
+                newImgId = v_index;
+
                 v_index = v_index.substr(v_index.indexOf("_")+1);
-                currentPhoto = v_index;
-                
-                bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
-                $('#bigText').text(data[currentPhoto].name);
+                newPhoto = v_index;
 
-                console.log(currentPhoto);
+                fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
                 
-                //console.log(data.length);
+
             });    
 
-            $("#bigImage").click(() => {
-               ;
-            });    
+            $("#rightArrow").click(() => {
 
-            bigImage.src = 'data:image/png;base64,' + data[currentPhoto].img.data.$binary.base64;
-            $('#bigText').text(data[currentPhoto].name);
+                newPhoto = $("#current").text();
+                newImgId = "img_"+$("#current").text();
+
+                currentImgId = newImgId;
+                currentPhoto = newPhoto;
+
+                if ( newPhoto <= data.length-2 ) {
+                    ++newPhoto;
+                    newImgId = "img_" + newPhoto.toString();
+                }
+                else {
+                    newPhoto=0;
+                    newImgId = "img_" + newPhoto.toString();
+                }
+
+                fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
+
+            });
+
+            $("#leftArrow").click(() => {
+
+                newPhoto = $("#current").text();
+                newImgId = "img_"+$("#current").text();
+
+                currentImgId = newImgId;
+                currentPhoto = newPhoto;
+
+
+                if (newPhoto != 0) {
+                    --newPhoto;
+                    newImgId = "img_" + newPhoto.toString();
+                }
+                else {
+                    newPhoto=data.length-1;
+                    newImgId = "img_" + newPhoto.toString();
+                }
+
+
+
+                fillbigImage(newPhoto, newImgId, currentPhoto, currentImgId);
+
+            });
+            
 
 
 // a kép áttűnésnél járok
